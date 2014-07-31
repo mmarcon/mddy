59336cb46aa3ab30c133d42826466cfb9b8e816c
Massimiliano Marcon <massimiliano.marcon@here.com>
2014-07-31 15:16:26 +0200

    Initial cleanup

--- index.js
+++ index.js
@@ -1,86 +1,19 @@
 var express = require('express'),
     marked = require('marked'),
-    sass = require('node-sass'),
-    highlight = require('highlight.js'),
-    fs = require('fs'),
-    path = require('path'),
-    findit = require('findit'),
-    config = require('./config.json');
-
-var markedRenderer = new marked.Renderer();
-
-markedRenderer.image = function(href, title, altText){
-    var html = '';
-    html += '<div class="image-wrapper">';
-    html += '<img src="' + href + '" ';
-    if(title) {
-        html += 'title="' + title + '" ';
-    }
-    html += 'alt="' + altText + '"/>';
-    html += '</div>';
-    return html;
-};
-
-marked.setOptions({
-    renderer: markedRenderer,
-    gfm: true,
-    tables: true,
-    breaks: false,
-    pedantic: false,
-    sanitize: true,
-    smartLists: true,
-    smartypants: false,
-    highlight: function(code){
-        return highlight.highlightAuto(code).value;
-    }
-});
+    middleware = require('./lib/middleware'),
+    config = require('./config.json'),
+    controller = require('./lib/controller');
 
 var app = express();
+var root = __dirname;
+
 app.engine('.html', require('ejs').__express);
-app.use(sass.middleware({
-    src: __dirname + '/static',
-    dest: __dirname + '/static',
-    debug: false,
-    outputStyle: 'compressed'
-}));
-app.use(express.static(__dirname + '/static'));
-app.set('views', __dirname + '/templates');
+app.use(middleware.sass(root));
+app.use(express.static(root + '/static'));
+app.set('views', root + '/templates');
 app.set('view engine', 'html');
 
-app.get('/', function(req, res){
-    var index = {
-        user: config.title,
-        notes: []
-    };
-
-    var notesPath = path.join(__dirname, 'notes');
-
-    var finder = findit(notesPath);
-    finder.on('file', function(file, stat){
-        index.notes.push({
-            name: path.relative(notesPath, file),
-            url: '/' + path.relative(notesPath, file)
-        });
-    });
-    finder.on('end', function(){
-        res.render('index', index);
-    });
-});
-
-app.get('/*', function(req, res){
-    var fileName = req.params[0],
-        filePath = path.join(__dirname, 'notes', fileName);
-
-    fs.readFile(filePath, {encoding: 'utf-8'}, function(err, data){
-        if(err) {
-            return res.send(404);
-        }
-        var repo = {
-            title: fileName,
-            body: marked(data)
-        };
-        res.render('repo', repo);
-    });
-});
+app.get('/', controller.main(config, root));
+app.get('/*', controller.file(config, root));
 
 app.listen(process.env.PORT || process.env.VCAP_APP_PORT || 3000);
